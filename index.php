8b45a87da10977f491f3c8865036b0033968bbad
TofinDev <39640205+TofinDev@users.noreply.github.com>
2018-05-27 12:52:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,8 @@ $json = file_get_contents('http://data.tmd.go.th/api/WeatherToday/V1/?type=json'
 $obj = json_decode($json);
 
 //print_r($obj);
-
-echo $obj->timeFrameStart;
 echo $obj->Stations[0]->Temperature;
+echo "ติดต่อไม่ได้";
 echo "<br>";
 
 ?>
