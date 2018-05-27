a97b0a8ced3477e730dac6dc3238afb641d23a63
TofinDev <39640205+TofinDev@users.noreply.github.com>
2018-05-27 11:53:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,8 @@
-<?php    header ('Content-type: text/html; charset=utf-8');
+<?php
+$json = file_get_contents('http://data.tmd.go.th/api/WeatherToday/V1');
+$obj = json_decode($json);
 
-        $ch = curl_init(); 
+print_r($obj);
 
-        // set url สำหรับดึงข้อมูล 
-        curl_setopt($ch, CURLOPT_URL, "http://data.tmd.go.th/api/WeatherToday/V1/?type=json"); 
-
-        //return the transfer as a string 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-
-        // ตัวแปร $output เก็บข้อมูลทั้งหมดที่ดึงมา 
-        $output = curl_exec($ch); 
-        
-        // ปิดการเชื่อต่อ
-        curl_close($ch);    
-        // output ออกไปครับ
-        echo $output;
-        
-       
-                 
-  
 
 ?>
