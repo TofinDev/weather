cbf2738de1bd7c157ddc97ee8588a1dbca5848fc
TofinDev <39640205+TofinDev@users.noreply.github.com>
2018-05-27 12:07:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $json = file_get_contents('http://data.tmd.go.th/api/WeatherToday/V1');
 $obj = json_decode($json);
 
-print_r($obj);
+//print_r($obj);
 
 echo $obj->StationNameTh;
 echo $obj->Stations[0]->Temperature;
