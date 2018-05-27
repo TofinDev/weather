1ad02bc23341403e63197e9df5e6163ed8df7f2f
TofinDev <39640205+TofinDev@users.noreply.github.com>
2018-05-27 12:34:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 echo "Web alrady now";
 echo "<br>";
 
-$json = file_get_contents('http://api.football-data.org/v1/fixtures');
+$json = file_get_contents('https://api.coindesk.com/v1/bpi');
 $obj = json_decode($json);
 
 print_r($obj);
 
-echo $obj->StationNameTh;
-echo $obj->Stations[0]->Temperature;
-echo "<br>";
+//echo $obj->timeFrameStart;
+//echo $obj->Stations[0]->Temperature;
+//echo "<br>";
 
 ?>
